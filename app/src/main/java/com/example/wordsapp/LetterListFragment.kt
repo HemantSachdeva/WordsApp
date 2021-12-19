63ef1fda01e7fb17a9ae0a4770d7346e8581a696
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-12-19 13:35:22 +0530

    letterFragment: Display options menu and inflate view
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -1,6 +1,11 @@
 package com.example.wordsapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.wordsapp.databinding.FragmentLetterListBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -8,5 +13,20 @@ import androidx.fragment.app.Fragment
  * create an instance of this fragment.
  */
 class LetterListFragment : Fragment() {
+    private var _binding: FragmentLetterListBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLetterListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
